3c736a2fcb2d48eb9a0e6964abe373c73a4687cd
Ivan <ivanzdankin91@gmail.com>
2026-05-06 00:41:58 +0300

    Add SQlite database 0.3.0

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,21 +1,35 @@
 const express = require('express');
+const { initDb } = require('./db/initDb');
+
 const eventsRoutes = require('./routes/events.routes');
 const usersRoutes = require('./routes/users.routes');
-const { logger } = require('./middleware/logger.middleware');;
+const registrationsRoutes = require('./routes/registrations.routes');
+
+const { logger } = require('./middleware/logger.middleware');
 const { errorHandler } = require('./middleware/error.middleware');
 
 const app = express();
-
 app.use(express.json());
-
 app.use(logger);
 
 app.use('/api/events', eventsRoutes);
 app.use('/api/users', usersRoutes);
+app.use('/api/registrations', registrationsRoutes);
 
 app.use(errorHandler);
 
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`API started on http://localhost:${PORT}`);
-});
+
+async function start() {
+    try {
+        await initDb(); 
+        app.listen(PORT, () => {
+            console.log(`API started on http://localhost:${PORT}`);
+        });
+    } catch (err) {
+        console.error("Fatal error during startup:", err);
+        process.exit(1);
+    }
+}
+
+start();
