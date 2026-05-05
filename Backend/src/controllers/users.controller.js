aed1d8274d636f359a5f452b2a3a4771e5b229c6
Ivan <ivanzdankin91@gmail.com>
2026-05-06 00:38:01 +0300

    Add SQlite database 0.3.0

--- Backend/src/controllers/users.controller.js
+++ Backend/src/controllers/users.controller.js
@@ -1,37 +1,33 @@
 const usersService = require('../services/users.service');
 
 module.exports = {
-  getAll: (req, res, next) => {
+  getAll: async (req, res, next) => {
     try {
-      const items = usersService.getAllUsers();
+      const items = await usersService.getAllUsers();
       res.status(200).json({ items });
     } catch (err) { next(err); }
   },
-  
-  getById: (req, res, next) => {
+  getById: async (req, res, next) => {
     try {
-      const item = usersService.getUserById(req.params.id);
+      const item = await usersService.getUserById(req.params.id);
       res.status(200).json(item);
     } catch (err) { next(err); }
   },
-  
-  create: (req, res, next) => {
+  create: async (req, res, next) => {
     try {
-      const newItem = usersService.createUser(req.body);
+      const newItem = await usersService.createUser(req.body);
       res.status(201).json(newItem);
     } catch (err) { next(err); }
   },
-  
-  update: (req, res, next) => {
+  update: async (req, res, next) => {
     try {
-      const updatedItem = usersService.updateUser(req.params.id, req.body);
+      const updatedItem = await usersService.updateUser(req.params.id, req.body);
       res.status(200).json(updatedItem);
     } catch (err) { next(err); }
   },
-  
-  delete: (req, res, next) => {
+  delete: async (req, res, next) => {
     try {
-      usersService.deleteUser(req.params.id);
+      await usersService.deleteUser(req.params.id);
       res.status(204).send();
     } catch (err) { next(err); }
   }
