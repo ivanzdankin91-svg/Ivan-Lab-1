a3feca1774f4615bfdaadbbe095111b71692cfbd
Ivan <ivanzdankin91@gmail.com>
2026-05-06 00:23:29 +0300

    Add SQlite database 0.3.0

--- Backend/src/repositories/events.repository.js
+++ Backend/src/repositories/events.repository.js
@@ -1,32 +1,31 @@
-let events = [];
-let nextId = 1;
+const { all, get, run } = require('../db/dbClient');
+
+function escape(s) {
+    return String(s).replace(/'/g, "''");
+}
 
 module.exports = {
-  getAll: () => events,
-  
-  getById: (id) => events.find(e => e.id === id),
-  
-  add: (data) => {
-    const newEvent = { id: nextId++, ...data };
-    events.push(newEvent);
-    return newEvent;
-  },
-  
-  update: (id, data) => {
-    const index = events.findIndex(e => e.id === id);
-    if (index !== -1) {
-      events[index] = { ...events[index], ...data };
-      return events[index];
+    getAll: async (filter = {}, sortBy = 'id', order = 'DESC') => {
+        let sql = `SELECT * FROM Events`;
+        if (filter.location) sql += ` WHERE location = '${escape(filter.location)}'`;
+        sql += ` ORDER BY ${sortBy} ${order}`;
+        return await all(sql);
+    },
+
+    getById: async (id) => {
+        return await get(`SELECT * FROM Events WHERE id = ${Number(id)};`);
+    },
+
+    add: async (data) => {
+        const result = await run(`
+            INSERT INTO Events (title, date, location, capacity, description)
+            VALUES ('${escape(data.title)}', '${escape(data.date)}', '${escape(data.location)}', ${Number(data.capacity)}, '${escape(data.description || '')}');
+        `);
+        return await get(`SELECT * FROM Events WHERE id = ${result.lastID};`); 
+    },
+
+    delete: async (id) => {
+        const result = await run(`DELETE FROM Events WHERE id = ${Number(id)};`);
+        return result.changes > 0; 
     }
-    return null;
-  },
-  
-  delete: (id) => {
-    const index = events.findIndex(e => e.id === id);
-    if (index !== -1) {
-      events.splice(index, 1);
-      return true;
-    }
-    return false;
-  }
 };
