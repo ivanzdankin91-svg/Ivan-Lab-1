28e1398f822c7413a30260e708a240fc34a9c110
Ivan <ivanzdankin91@gmail.com>
2026-06-05 00:28:00 +0300

    Впровадження механізмів захисту від SQLi, XSS, IDOR та HTTP-заголовків

--- Backend/src/routes/events.routes.js
+++ Backend/src/routes/events.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const eventsController = require('../controllers/events.controller');
+const { demoAuth } = require('../middleware/auth.middleware');
 
 router.get('/', eventsController.getAll);
 router.get('/:id', eventsController.getById);
-router.post('/', eventsController.create);
-router.put('/:id', eventsController.update);
-router.delete('/:id', eventsController.delete);
+
+router.post('/', demoAuth, eventsController.create);
+router.put('/:id', demoAuth, eventsController.update);
+router.delete('/:id', demoAuth, eventsController.delete);
 
 module.exports = router;
